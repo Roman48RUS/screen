b831cf6a670bf3704656b11aa8382066797d5642
Roman48RUS <realroman445@gmail.com>
2021-04-26 17:28:46 +0300

    basic functionality

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, redirect, request, make_response, session, url_for, \
-    send_from_directory
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__, static_folder="/")
 app.config["SECRET_KEY"] = "secret_key"
+
+
+i = 0
 
 
 class Image:
@@ -17,20 +19,32 @@ class Image:
         return self.image
 
 
-@app.route("/", methods=["GET", "POST"])
+def test():
+    global i
+    i = (i + 1) % 2
+    return i
+
+
+@app.route("/", methods=["GET"])
 def index():
     default = None
     images = []
     f = open("cfg/time.cfg")
     for elem in f.readlines():
-        if not elem.strip()[0] == ";":
-            args = elem.split()
-            if not args[1] == "default":
-                img = Image("/images/" + args[0], args[1])
-                images.append(img)
-            else:
-                default = args[0]
-    return render_template("index.html", images=images, default=default)
+        if elem.strip():
+            if not elem.strip()[0] == ";":
+                args = elem.split()
+                if not args[1] == "default":
+                    img = Image("/images/" + args[0], args[1])
+                    images.append(img)
+                else:
+                    default = args[0]
+    return render_template("index.html", images=enumerate(images), default=default)
+
+
+@app.route("/api/current_image", methods=["GET"])
+def get_image():
+    return jsonify({"image": str(test())})
 
 
 if __name__ == "__main__":
