2f395490fd0429c469fe464a3ca18d68f26e950c
Roman48RUS <realroman445@gmail.com>
2021-04-26 20:10:58 +0300

    funcional app

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, jsonify
+from datetime import time, datetime
 
 app = Flask(__name__, static_folder="/")
 app.config["SECRET_KEY"] = "secret_key"
-
-
-i = 0
+j = 0
 
 
 class Image:
-    def __init__(self, image, time):
+    def __init__(self, image, time, number):
         self.image = image
         self.time = time
+        self.number = number
 
     def get_time(self):
         return self.time
@@ -18,33 +18,53 @@ class Image:
     def get_image(self):
         return self.image
 
-
-def test():
-    global i
-    i = (i + 1) % 2
-    return i
+    def get_number(self):
+        return self.number
 
 
-@app.route("/", methods=["GET"])
-def index():
+def get_images():
+
     default = None
     images = []
     f = open("cfg/time.cfg")
+    i = 0
     for elem in f.readlines():
         if elem.strip():
             if not elem.strip()[0] == ";":
                 args = elem.split()
                 if not args[1] == "default":
-                    img = Image("/images/" + args[0], args[1])
+                    hours = int(args[1].split(":")[0])
+                    minutes = int(args[1].split(":")[1])
+                    seconds = int(args[1].split(":")[2])
+                    img = Image("/images/" + args[0], time(hours, minutes, seconds), i)
+                    i += 1
                     images.append(img)
                 else:
-                    default = args[0]
+                    default = "/images/" + args[0]
+                    i += 1
+    return images, default
+
+
+def test():
+    global j
+    j = (j + 1) % 2
+    return j
+
+
+@app.route("/", methods=["GET"])
+def index():
+    images, default = get_images()
     return render_template("index.html", images=enumerate(images), default=default)
 
 
 @app.route("/api/current_image", methods=["GET"])
 def get_image():
-    return jsonify({"image": str(test())})
+    for elem in get_images()[0]:
+        if (elem.time.hour == datetime.now().hour
+                and elem.time.minute == datetime.now().minute
+                and elem.time.second == datetime.now().second):
+            return jsonify({"image": str(elem.get_number())})
+    return jsonify({"image": ""})
 
 
 if __name__ == "__main__":
